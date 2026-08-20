b99f59032a115187ced801e0a6b5ed59c4d50f88
SVLoneStar <45231728+SVLoneStar@users.noreply.github.com>
2026-08-21 00:39:05 +0200

    Set clean firmware build identifier

--- Version.h
+++ Version.h
@@ -3,8 +3,8 @@
 // Arduino IDE does not provide a project-local pre-build hook for Git metadata.
 // Update the hash and manual dirty flag as described in README.md before builds.
 #define FIRMWARE_VERSION "0.1.0-dev"
-#define FIRMWARE_GIT_HASH "c8445b1"
-#define FIRMWARE_BUILD_DIRTY 1
+#define FIRMWARE_GIT_HASH "9c3221c"
+#define FIRMWARE_BUILD_DIRTY 0
 #define FIRMWARE_BUILD_DATE __DATE__
 #define FIRMWARE_BUILD_TIME __TIME__
 
